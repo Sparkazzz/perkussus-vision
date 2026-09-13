416bc24291cc0aed903598235a315d14e32f5ff5
Sparkazzz <alessio.gentile90@icloud.com>
2026-09-13 22:15:19 +0200

    Prioritize fast POI interaction layer

--- bootstrap.mjs
+++ bootstrap.mjs
@@ -1,5 +1,6 @@
 await import('./map-bridge.mjs?v=2');
 await import('./app.mjs?v=5');
 await import('./ui-controller.mjs?v=2');
+await import('./poi-tap-fix.mjs?v=2');
+if(window.PV_POI_TAP_READY)await Promise.race([window.PV_POI_TAP_READY,new Promise(r=>setTimeout(r,2500))]);
 await import('./earth-explorer.mjs?v=2');
-await import('./poi-tap-fix.mjs?v=1');
