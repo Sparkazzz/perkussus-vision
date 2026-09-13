ba66394af1ff2fea8c3e60834f9490f56f6d5927
Sparkazzz <alessio.gentile90@icloud.com>
2026-09-13 22:04:41 +0200

    Load POI mobile tap fix

--- bootstrap.mjs
+++ bootstrap.mjs
@@ -2,3 +2,4 @@ await import('./map-bridge.mjs?v=2');
 await import('./app.mjs?v=5');
 await import('./ui-controller.mjs?v=2');
 await import('./earth-explorer.mjs?v=2');
+await import('./poi-tap-fix.mjs?v=1');
