a7f3cf3fb8fb652535f62dde69ca1ad3e9033976
Sparkazzz <alessio.gentile90@icloud.com>
2026-09-13 22:36:33 +0200

    Use stable base POI enrichment module

--- bootstrap.mjs
+++ bootstrap.mjs
@@ -5,4 +5,4 @@ await import('./poi-tap-fix.mjs?v=2');
 if(window.PV_POI_TAP_READY)await Promise.race([window.PV_POI_TAP_READY,new Promise(r=>setTimeout(r,2500))]);
 await import('./earth-explorer.mjs?v=2');
 await import('./category-controller-v4.mjs?v=1');
-await import('./poi-sheet-upgrade-v5.mjs?v=1');
+await import('./poi-sheet-upgrade-v6.mjs?v=1');
